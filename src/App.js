a6947cb965c2d6b08998b0baabb0db282c67eb58
devWaz <fawazodessy@gmail.com>
2022-06-23 14:07:31 +0100

    yeah

--- src/App.js
+++ src/App.js
@@ -35,38 +35,41 @@ class App extends Component {
   
     showPersonHandler = () =>{
       const doesShow = this.state.showPerson
-
       this.setState({
         showPerson : !doesShow
       })
+    }
+
+    deletePersonHandler = (personindex) =>{
+        const Del = this.state.persons;
+        
+
     }
 
   render(){
 
     let people = null;
     if(this.state.showPerson){
-       people = <div>
-        <Person 
-         changed = {this.nameChangedHandler}
-         name = {this.state.persons[0].name}
-         age = {this.state.persons[0].age}/>
-         <Person
-         name = {this.state.persons[1].name}
-         age = {this.state.persons[1].age}/>
-         <Person
-         name = {this.state.persons[2].name}
-         age = {this.state.persons[2].age}/>
-        </div> 
-    }
+       people = ( 
+      <div>
+       {this.state.persons.map((pesin , index) =>{
+          return <Person
+            name = {pesin.name}
+            age = {pesin.age}
+            clicked = {() => this.deletePersonHandler(index)}
+            changed = {this.nameChangedHandler}
+          />
+        })
+       }
+      </div> 
+    )
+       }
 
   return (
     <div className="App">
         <h1>Hello, World! I'm devWaz</h1>
         <button onClick={this.showPersonHandler}> Switch </button>
         {people}
-        
-     
-
     </div>
   );
 }
